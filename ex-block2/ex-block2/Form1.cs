e792d329c98a8b4f03f468ba4f2bf2da29cf794f
Mytnyk <mytnyk.dmytro1124@vu.cdu.edu.ua>
2025-12-27 17:07:16 +0200

    Add logic to Form1

--- ex-block2/ex-block2/Form1.cs
+++ ex-block2/ex-block2/Form1.cs
@@ -4,7 +4,14 @@ namespace ex_block2
     {
         public Form1()
         {
+
             InitializeComponent();
+            button1.Text = "Старт обробки";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
