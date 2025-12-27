1d37c963c61284ecebfffe00d0c9e68ccc6f7a67
Mytnyk <mytnyk.dmytro1124@vu.cdu.edu.ua>
2025-12-27 17:16:18 +0200

    Renamed som var and add logic

--- ex-block2/ex-block2/Form1.cs
+++ ex-block2/ex-block2/Form1.cs
@@ -15,18 +15,49 @@ namespace ex_block2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string targetDir = @"C:\Users\MNYAW\Desktop\lab6_exception_block-2\ex-block2\ex-block2\bin\Debug\net9.0-windows\photos";
+            string folder = @"C:\Users\MNYAW\Desktop\lab6_exception_block-2\ex-block2\ex-block2\bin\Debug\net9.0-windows\photos";
 
-            if (!Directory.Exists(targetDir))
+            if (!Directory.Exists(folder))
             {
-                MessageBox.Show($"Папку не знайдено:\n{targetDir}", "Неправильно вписаний шлях", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Папку не знайдено:\n{folder}", "Неправильно вписаний шлях", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            string[] files = Directory.GetFiles(targetDir);
+            string[] files = Directory.GetFiles(folder);
             Regex regexExtForImage = new Regex(@"^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
 
-            int processedCount = 0;
+            int cnt = 0;
+
+            foreach (string filePath in files)
+            {
+                if (filePath.Contains("-mirrored.gif")) continue;
+
+                try
+                {
+                    using (Bitmap bmp = new Bitmap(filePath))
+                    {
+                        bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                        string fileNameNoExt = Path.GetFileNameWithoutExtension(filePath);
+                        string newFileName = fileNameNoExt + "-mirrored.gif";
+                        string savePath = Path.Combine(folder, newFileName);
+                        bmp.Save(savePath, ImageFormat.Gif);
+
+                        cnt++;
+                    }
+                }
+                catch
+                {
+                    string ext = Path.GetExtension(filePath).TrimStart('.');
+                    if (regexExtForImage.IsMatch(ext))
+                    {
+                        MessageBox.Show(
+                            $"Знайдено битий файл: '{Path.GetFileName(filePath)}'\n(правильне розширення - дивний вміст).",
+                            "Увага, помилка файлу!",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning);
+                    }
+                }
+            }
 
         }
     }
