a1cfe17dd4b7ebd36c6b53d1f57121bf1e5f509d
Mytnyk <mytnyk.dmytro1124@vu.cdu.edu.ua>
2025-12-27 17:11:54 +0200

    Add usings and some logit into the button

--- ex-block2/ex-block2/Form1.cs
+++ ex-block2/ex-block2/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+
 namespace ex_block2
 {
     public partial class Form1 : Form
@@ -11,6 +15,18 @@ namespace ex_block2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string targetDir = @"C:\Users\MNYAW\Desktop\lab6_exception_block-2\ex-block2\ex-block2\bin\Debug\net9.0-windows\photos";
+
+            if (!Directory.Exists(targetDir))
+            {
+                MessageBox.Show($"Папку не знайдено:\n{targetDir}", "Неправильно вписаний шлях", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] files = Directory.GetFiles(targetDir);
+            Regex regexExtForImage = new Regex(@"^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+
+            int processedCount = 0;
 
         }
     }
